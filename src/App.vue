91a149016726741bbf5334a998553607ca596a2b
Partithura <jorgebrunoalmeidadossantos@gmail.com>
2023-02-02 11:19:09 -0300

    🧑‍💻 Computed e Watch em Composition API
    Criação de um método computed responsável por gerar o nome completo e de um método watch que monitore alterações no first_name

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" @click="changeName()">
-  {{ name }}
-  <hr>
   {{ user.first_name }} {{ user.last_name }}
-  <br>
-  {{ admin.first_name }} {{ admin.last_name }}
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <br />
+  {{ fullName }}
+  <button @click="user.first_name = 'Bruno'">Atualizar</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import { ref, reactive } from 'vue';
+import { ref, computed, watch } from 'vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
   setup() {
-    const user = reactive({
+    const user = ref({
       first_name: 'Jorge',
       last_name: 'Almeida'
     });
+  
+    const fullName = computed(() => `${user.value.first_name} ${user.value.last_name}`);
 
-    const admin = ref({
-      first_name: 'Admin',
-      last_name: 'Master'
-    });
-
-    let name = 'Bruno';
-
-    const changeName = () => {
-      name = 'Bruno dos Santos';
-      user.first_name = 'Claudio';
-      admin.value.last_name = 'XXX';
-    };
+    watch(() => user.value.first_name, () => {
+      console.log('Primeiro nome foi alterado');
+    })
 
     return {
-      name,
-      changeName,
       user,
-      admin
-    }
+      fullName,
+    };
   }
 }
 </script>
